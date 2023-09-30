782caee83c6bf9f0c419be132ac94344357d2018
Chaitra Suvarna <chaitrasuvarna4065@gmail.com>
2023-09-30 10:25:47 +0530

    Added Login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Signup from "./pages/Signup.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
-export default function Signup() {
+export default function Login() {
     return (
+
         <div className="flex justify-center item-center flex-1 h-screen">
             <h3 className="text-slate-950 font-medium text-xl md:text-slate-200 absolute top-8 left-8 ">
                 Lock&amp;Key
@@ -11,8 +12,8 @@ export default function Signup() {
             </div>
             <div className=" flex flex-1 bg-blue-50 bg-center justify-center items-center">
                 <form className="md:w-1/2 p-8 md:p-0 space-y-5 flex flex-col justify-center">
-                    <h1 className="text-xl font-semibold">Sign Up</h1>
-                    <p>Please fill in this form to create an account.</p>
+                    <h1 className="text-xl font-semibold">Login</h1>
+                    <p>To keep connecting with us please login with the correct details.</p>
                     <hr />
                     <div>
                         <label htmlFor="email" className="inline-block border-0 mb-2 ">
@@ -39,49 +40,38 @@ export default function Signup() {
                         />
                     </div>
 
-                    <div>
-                        <label htmlFor="psw-repeat" className="w-9/12 inline-block mb-2">
-                            <p className="font-medium text-lg">Repeat Password</p>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Repeat Password"
-                            name="psw-repeat"
-                            required
-                            className="bg-inherit border-2 inline-block px-8 py-6 border-slate-300 focus:outline-none focus:border-b-4 focus:border-b-green-400 h-7   w-full"
-                        />
-                    </div>
-                    <div className="flex space-x-2">
-                        <input
-                            type="checkbox"
-                            defaultChecked="checked"
-                            name="remember"
-                            className="inline-block"
-                        />
-                        <p>Remember me?</p>
+
+                    <div className="flex justify-between w-full">
+                        <div className="flex space-x-2">
+                            <input
+                                type="checkbox"
+                                defaultChecked="checked"
+                                name="remember"
+                                className="inline-block"
+                            />
+                            <p>Remember me?</p>
+                        </div>
+                        <a href="" className="text-blue-700">
+                            Forgot Password
+                        </a>
                     </div>
 
-                    {/* <p>
-            By creating an account you agree to our{" "}
-            <a href="#" style={{ color: "dodgerblue" }}>
-              Terms &amp; Privacy
-            </a>
-            .
-          </p> */}
+
                     <div className="clearfix">
                         <button
                             type="submit"
                             className="bg-blue-600 text-white  mb-2 rounded w-full hover:bg-blue-700 opacity-90 cursor-pointer px-6 py-4"
                         >
-                            Sign Up
+                            Login
                         </button>
                     </div>
                     <p>
-                        Already have an account?{" "}
-                        <Link to={"/login"} className="text-blue-700 text-secondary font-medium text-lg">
-                            Login
+                        Don't have an account?{" "}
+                        <Link to={"/signup"} className="text-blue-700 text-secondary font-medium text-lg">
+                            Signup
                         </Link>
                     </p>
+
                 </form>
             </div>
         </div>
