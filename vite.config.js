419387488ad40fead6447a928e387eeaac5114a0
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2024-02-06 09:45:54 +0530

    Made changes for deployement

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import http from "https"
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 
@@ -32,7 +32,9 @@ export default defineConfig(({ mode }) => {
     proxy: {
       '/api': {
         target: processEnvValues['process.env'].VITE_API_URL,
-       
+        changeOrigin: true,
+        secure: false,
+        agent: new http.Agent()
       }
     }
   }
